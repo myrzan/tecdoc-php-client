a5a054aee3ec93a916be2d9e707c7b82b629bdd5
Stephan Baumeister <stephan@baumeister.name>
2020-05-29 18:03:54 +0200

    WIP: implement more TecDoc API functions.

--- src/Client.php
+++ src/Client.php
@@ -4,14 +4,23 @@
 namespace Baumeister\TecDocClient;
 
 
+use Baumeister\TecDocClient\Generated\GetAmBrands;
+use Baumeister\TecDocClient\Generated\GetAmBrandsResponse;
+use Baumeister\TecDocClient\Generated\GetArticleLinkedAllLinkingTarget4;
+use Baumeister\TecDocClient\Generated\GetArticleLinkedAllLinkingTarget4Response;
+use Baumeister\TecDocClient\Generated\GetArticleLinkedAllLinkingTargetsByIds3;
+use Baumeister\TecDocClient\Generated\GetArticleLinkedAllLinkingTargetsByIds3Response;
 use Baumeister\TecDocClient\Generated\GetArticles;
 use Baumeister\TecDocClient\Generated\GetArticlesResponse;
 use Baumeister\TecDocClient\Generated\GetLanguages;
 use Baumeister\TecDocClient\Generated\GetLanguagesResponse;
+use Baumeister\TecDocClient\Generated\GetVehicleByIds3;
+use Baumeister\TecDocClient\Generated\GetVehicleByIds3Response;
 use GuzzleHttp\Client as GuzzleClient;
 use JsonMapper;
 use ReflectionClass;
 use RuntimeException;
+use stdClass;
 
 class Client
 {
@@ -30,16 +39,42 @@ class Client
         $this->jsonMapper = new JsonMapper();
     }
 
-    public function getLanguages(GetLanguages $paramsObject) : GetLanguagesResponse
+    public function getLanguages(GetLanguages $paramsObject): GetLanguagesResponse
     {
         $json = $this->call('getLanguages', $paramsObject);
         return $this->jsonMapper->map($json, new GetLanguagesResponse());
     }
 
-    public function getArticles(GetArticles $paramsObject) : GetArticlesResponse
+    public function getAmBrands(GetAmBrands $paramsObject): GetAmBrandsResponse
+    {
+        $json = $this->call('getAmBrands', $paramsObject);
+        return $this->jsonMapper->map($json, new GetAmBrandsResponse());
+    }
+
+    public function getArticles(GetArticles $paramsObject): GetArticlesResponse
     {
         $json = $this->call('getArticles', $paramsObject);
         return $this->jsonMapper->map($json, new GetArticlesResponse());
+    }
+
+    public function getVehicleByIds3(GetVehicleByIds3 $paramsObject): GetVehicleByIds3Response
+    {
+        Client::addIntermediatePropNamedArray($paramsObject, 'carIds');
+        $json = $this->call('getVehicleByIds3', $paramsObject);
+        return $this->jsonMapper->map($json, new GetVehicleByIds3Response());
+    }
+
+    public function getArticleLinkedAllLinkingTargetsByIds3(GetArticleLinkedAllLinkingTargetsByIds3 $paramsObject): GetArticleLinkedAllLinkingTargetsByIds3Response
+    {
+        Client::addIntermediatePropNamedArray($paramsObject, 'linkedArticlePairs');
+        $json = $this->call('getArticleLinkedAllLinkingTargetsByIds3', $paramsObject);
+        return $this->jsonMapper->map($json, new GetArticleLinkedAllLinkingTargetsByIds3Response());
+    }
+
+    public function getArticleLinkedAllLinkingTarget4(GetArticleLinkedAllLinkingTarget4 $paramsObject): GetArticleLinkedAllLinkingTarget4Response
+    {
+        $json = $this->call('getArticleLinkedAllLinkingTarget4', $paramsObject);
+        return $this->jsonMapper->map($json, new GetArticleLinkedAllLinkingTarget4Response());
     }
 
     private function call(string $functionName, $paramsObject)
@@ -53,30 +88,60 @@ class Client
         ]);
         if ($response->getStatusCode() == 200) {
             $json = json_decode($response->getBody());
-            if (isset($json->data->array)) {
-                $json->data = $json->data->array;
-                unset($json->data->array);
-            }
+            Client::recursivelyRemoveIntermediatePropsNamedArray($json);
             return $json;
         }
         throw new RuntimeException("HTTP request failed with code {$response->getStatusCode()}");
     }
 
-    private static function paramsObjectToArray($object) : array {
+    private static function recursivelyRemoveIntermediatePropsNamedArray($obj, $parentObj = null, $propName = null)
+    {
+        foreach ($obj as $prop => $val) {
+            if ($prop === 'array' && $parentObj != null && $propName != null) {
+                $parentObj->$propName = $val;
+                unset($obj->array);
+            }
+            if (is_object($val) or is_array($val)) {
+                Client::recursivelyRemoveIntermediatePropsNamedArray($val, $obj, $prop);
+            }
+        }
+    }
+
+    private static function addIntermediatePropNamedArray(object $paramsObject, string $propName): void
+    {
+        $reflectionClass = new ReflectionClass($paramsObject);
+        $reflectionProperty = $reflectionClass->getParentClass()->getProperty($propName);
+        $reflectionProperty->setAccessible(true);
+        $reflectionProperty->setValue($paramsObject, ['array' => $reflectionProperty->getValue($paramsObject)]);
+    }
+
+    private static function paramsObjectToArray($object): array
+    {
         $reflectionClass = new ReflectionClass($object);
-        $array = [];
+        $result = [];
         do {
             $properties = $reflectionClass->getProperties();
             foreach ($properties as $property) {
                 $property->setAccessible(true);
+                $propName = $property->getName();
                 $value = $property->getValue($object);
                 if (is_object($value)) {
-                    $array[$property->getName()] = self::paramsObjectToArray($value);
+                    $result[$propName] = self::paramsObjectToArray($value);
+                } else if (is_array($value)) {
+                    $result[$propName] = [];
+                    foreach ($value as $item) {
+                        if (is_object($item)) {
+                            $result[$propName][] = self::paramsObjectToArray($item);
+                        } else {
+                            $result[$propName][] = $item;
+                        }
+                    }
+
                 } else {
-                    $array[$property->getName()] = $value;
+                    $result[$propName] = $value;
                 }
             }
         } while ($reflectionClass = $reflectionClass->getParentClass());
-        return $array;
+        return $result;
     }
 }
--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -3,8 +3,13 @@
 namespace Baumeister\TecDocClient\Tests;
 
 use Baumeister\TecDocClient\Client;
+use Baumeister\TecDocClient\Generated\GetAmBrands;
+use Baumeister\TecDocClient\Generated\GetArticleLinkedAllLinkingTarget4;
+use Baumeister\TecDocClient\Generated\GetArticleLinkedAllLinkingTargetsByIds3;
 use Baumeister\TecDocClient\Generated\GetArticles;
 use Baumeister\TecDocClient\Generated\GetLanguages;
+use Baumeister\TecDocClient\Generated\GetVehicleByIds3;
+use Baumeister\TecDocClient\Generated\LinkedArticlePairType;
 use PHPUnit\Framework\TestCase;
 
 class ClientTest extends TestCase
@@ -23,9 +28,20 @@ class ClientTest extends TestCase
     {
         $params = (new GetLanguages())->setLang('de');
 
-        $getLanguagesResponse = $this->tecDocClient->getLanguages($params);
+        $response = $this->tecDocClient->getLanguages($params);
 
-        print_r($getLanguagesResponse);
+        print_r($response);
+    }
+
+    public function testGetAmBrands()
+    {
+        $params = (new GetAmBrands())
+            ->setLang('de')
+            ->setArticleCountry('de');
+
+        $response = $this->tecDocClient->getAmBrands($params);
+
+        print_r($response);
     }
 
     public function testGetArticles()
@@ -35,8 +51,70 @@ class ClientTest extends TestCase
             ->setArticleCountry('de')
             ->setDataSupplierIds([4]);
 
-        $getArticlesResponse = $this->tecDocClient->getArticles($params);
+        $response = $this->tecDocClient->getArticles($params);
 
-        print_r($getArticlesResponse);
+        print_r($response);
+    }
+
+    public function testGetArticleLinkedAllLinkingTargetsByIds3()
+    {
+        $params = (new GetArticleLinkedAllLinkingTargetsByIds3())
+            ->setLang('de')
+            ->setArticleCountry('de')
+            ->setArticleId(6784816)
+            ->setLinkingTargetType('P')
+            ->setNumberplateType(20)
+            ->setImmediateAttributs(true)
+            ->setLinkedArticlePairs([
+                (new LinkedArticlePairType())
+                    ->setArticleLinkId(6785022)
+                    ->setLinkingTargetId(1808),
+                (new LinkedArticlePairType())
+                    ->setArticleLinkId(6785053)
+                    ->setLinkingTargetId(1809),
+                (new LinkedArticlePairType())
+                    ->setArticleLinkId(6784973)
+                    ->setLinkingTargetId(1795),
+            ]);
+
+        $response = $this->tecDocClient->getArticleLinkedAllLinkingTargetsByIds3($params);
+
+        print_r($response);
+    }
+
+    public function testGetArticleLinkedAllLinkingTarget4()
+    {
+        $params = (new GetArticleLinkedAllLinkingTarget4())
+            ->setLang('de')
+            ->setCountry('de')
+            ->setArticleCountry('de')
+            ->setArticleId(6784816)
+            ->setLinkingTargetType('P');
+
+        $response = $this->tecDocClient->getArticleLinkedAllLinkingTarget4($params);
+
+        print_r($response);
+    }
+
+    public function testGetVehicleByIds3()
+    {
+        $params = (new GetVehicleByIds3())
+            ->setLang('de')
+            ->setArticleCountry('de')
+            ->setCountriesCarSelection('de')
+            ->setCountry('de')
+            ->setAxles(true)
+            ->setCabs(true)
+            ->setKbaData(true)
+            ->setMotorCodes(true)
+            ->setProtoTypes(true)
+            ->setRegistrationInfo(true)
+            ->setSecondaryTypes(true)
+            ->setWheelbases(true)
+            ->setCarIds([1808, 1809, 1795]);
+
+        $response = $this->tecDocClient->getVehicleByIds3($params);
+
+        print_r($response);
     }
 }
